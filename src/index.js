9bdd072548e027b30243b134fcce512ded9afcb5
Linar <>
2021-03-13 15:16:47 +0300

    update: edit index file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min () {
+  let number = arguments[0];
+  for (let i = 0; i < arguments.length; i++) {
+    if (arguments[i] < number) {
+      number = arguments[i];
+    }
+  }
+  return number
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max () {
+  let number = arguments[0];
+  for (let i = 0; i < arguments.length; i++) {
+    if (arguments[i] > number) {
+      number = arguments[i];
+    }
+  }
+  return number
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg () {
+  let numbers_sum = 0;
+  let count = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    numbers_sum += arguments[i];
+    count += 1
+  }
+return numbers_sum / count
 }
