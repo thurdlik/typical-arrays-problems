9079c853d68d55a8095fc36b620ea78baedddec8
Linar <>
2021-03-13 16:24:11 +0300

    feature: complete min max avg fn

--- src/index.js
+++ src/index.js
@@ -1,30 +1,39 @@
 
-exports.min = function min () {
-  let number = arguments[0];
-  for (let i = 0; i < arguments.length; i++) {
-    if (arguments[i] < number) {
-      number = arguments[i];
+exports.min = function min (array) {
+  if (arguments.length === 0 || array.length === 0) {
+    return 0;
+  }
+  let number = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < number) {
+      number = array[i];
     }
   }
-  return number
+  return number;
 }
 
-exports.max = function max () {
-  let number = arguments[0];
-  for (let i = 0; i < arguments.length; i++) {
-    if (arguments[i] > number) {
-      number = arguments[i];
+exports.max = function max (array) {
+  if (arguments.length === 0 || array.length === 0) {
+    return 0;
+  }
+  let number = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > number) {
+      number = array[i];
     }
   }
-  return number
+  return number;
 }
 
-exports.avg = function avg () {
+exports.avg = function avg (array) {
+  if (arguments.length === 0 || array.length === 0) {
+    return 0;
+  }
   let numbers_sum = 0;
   let count = 0;
-  for (let i = 0; i < arguments.length; i++) {
-    numbers_sum += arguments[i];
-    count += 1
+  for (let i = 0; i < array.length; i++) {
+    numbers_sum += array[i];
+    count += 1;
   }
-return numbers_sum / count
+  return numbers_sum / count;
 }
